98e450c343b5b5a8419ba84f92cbc1c4d74f5efc
Kegan Cochran <hmu@kegancochran.com>
2021-03-13 02:05:00 +0000

    edited readability

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -1,3 +1,14 @@
-// TODO: Ask for string of text
+#include <stdio.h>
+#include <string.h>
+#include <cs50.h>
 
-// TODO: Print grade level. Use "Below Grade 1" and "Grade 16+"/ 
+int main(void)
+{
+    // TODO: Ask for string of text.
+    string input = get_string("Text: \n");
+    
+    // TODO: Print grade level. Use "Below Grade 1" and "Grade 16+"/ 
+    // Coleman-Liau Index:
+    // index = 0.0588 * L - 0.296 * S - 15.8
+    printf("hello world");
+}
